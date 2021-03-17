b78fbf6ef81028f2e6e0cfcdb48d8a4d421d8d57
Kinjal Mugatwala <kmuga001@ucr.edu>
2021-03-16 23:37:07 -0700

    coded most of cross validation

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <stdio.h>
+#include <float.h>
+#include <math.h>
 
 using namespace std;
 
 void search_dataset(vector< vector<double> > data);
 bool checkFeatureInVector(vector<int> v, int val);
-int crossValidation();
+double leave_one_out_cross_validation(vector< vector<double> > data, vector<int> currentFeatures, int k);
 
 
 int main() {
@@ -52,7 +55,7 @@ void search_dataset(vector< vector<double> > data) { //remember that features st
         for(int j = 1; j < data[0].size(); j++) {
             if(checkFeatureInVector(currentFeatures, j) == false){ //check if the feature is in the currentFeatures set: false = no
                 cout << "Considering to add feature #" << j << endl;
-                int accuracy = crossValidation();
+                double accuracy = leave_one_out_cross_validation(data, currentFeatures, j);
 
                 if(accuracy > bestAccuracy){
                     bestAccuracy = accuracy;
@@ -76,6 +79,49 @@ bool checkFeatureInVector(vector<int> v, int val) {
     return false;
 }
 
-int crossValidation(){
-    return 0;
-} 
+double leave_one_out_cross_validation(vector< vector<double> > data, vector<int> currentFeatures, int k) {
+    vector<double> object_to_classify;
+    double num_correct = 0;
+    double label_object_to_classify = 0;
+    for(int i = 0; i < data.size(); i++) { //for each row/object: each row represents an object (classification: feature data)
+        object_to_classify = data[i];
+        object_to_classify.erase(object_to_classify.begin()); //pop front, so that we can get just the feature data part and not label
+        label_object_to_classify = data[i][0]; //first column of data
+
+        double nn_distance = DBL_MAX;
+        double nn_location = DBL_MAX;
+        double nn_label = 0;
+        //cout << "looping over i, at the " << i+1 << " location." << endl;
+        //cout << "the " << i+1 << "th object is in class " << label_object_to_classify << endl;
+        for(int k = 0; k < data.size(); k++){
+            if(k != i){ //don't compare to yourself
+                //calculate distance
+                double distance = 0;
+                vector<double> currData_features = data[k];
+                currData_features.erase(currData_features.begin());
+                double currData_label = data[k][0];
+
+                double sum = 0; //euclidean distance code here, need to loop to get sum of differences in data points
+                for(int j = 0; j < object_to_classify.size(); j++){
+                    double diff = object_to_classify.at(j) - currData_features.at(j);
+                    sum += pow(diff, 2);
+                }
+                distance = sqrt(sum);
+
+                if(distance < nn_distance){
+                    nn_distance = distance;
+                    nn_location = k;
+                    nn_label = data[nn_location][0];
+                }
+
+
+            }
+        }
+        if(label_object_to_classify == nn_label){ //check if the label was correct
+            num_correct += 1;
+        }
+
+    }
+    return num_correct/(data.size()); //returned accuracy
+
+}
